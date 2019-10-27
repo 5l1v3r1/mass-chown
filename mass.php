e57932f5a6040c3f885cb8f063b878a260bcc808
Sora Cyber Team <39432557+soracyberteam@users.noreply.github.com>
2019-10-27 15:02:47 -0400

    Update mass.php

--- mass.php
+++ mass.php
@@ -26,6 +26,10 @@ function head(){
 }
 head();
 if($argv[1] && $argv[2]){
+    echo "[*] $argv[2]:$argv[2] => $argv[1] ...";
+    system("chown $argv[2]:$argv[2] ../*");
+    system("chown $argv[2]:$argv[2] *");
+    sleep(3);
     buka($argv[1],$argv[2]);
 }else{
     echo "Usage : php mass.php [dir] [user]\nExample : php mass.php /home/ apache\n";
